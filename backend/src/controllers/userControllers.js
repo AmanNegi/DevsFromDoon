ded4cfd8bc36dc9cba1cae8e48fc0334c9197182
aashutosh148 <aashutoshsingh468847@gmail.com>
2024-09-07 22:54:44 +0530

    feat: routes-Tested errorResolved

--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -1,3 +1,5 @@
+const User = require('../models/User');
+
 exports.login = async (req, res) => {
 	const { registrationNo, password } = req.body;
 	if (!registrationNo || !password) {
@@ -7,7 +9,7 @@ exports.login = async (req, res) => {
 	}
 
 	try {
-		const user = await User.findOne({ registrationNo }).select('+password');
+		const user = await User.findOne({ registrationNo });
 
 		if (!user) {
 			return res.status(404).json({ message: 'Invalid credentials' });
@@ -23,3 +25,16 @@ exports.login = async (req, res) => {
 		return res.status(500).json({ message: err.message });
 	}
 };
+
+
+exports.register = async (req, res) => {
+    try {
+        const user = new User(req.body);
+        if(!user)return res.status(404).json({ message: 'Invalid Inputs' });
+        await user.save();
+        return res.status(200).json({ message: 'successfully saved' });
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+
+}
